0841305d38d48496783108d74be5ca0079b4cd95
sujicgi <yisumin0214@gmail.com>
2026-06-10 01:26:49 +0900

    Add database and charts

--- 서버.py
+++ 서버.py
@@ -1,20 +1,26 @@
 from flask import Flask, request, jsonify
 import sqlite3
+from flask import render_template
 
 app = Flask(__name__)
 
 def init_db():
-    conn = sqlite3.connect('simulation.db')
+    conn = sqlite3.connect("simulation.db")
     cur = conn.cursor()
 
-    cur.execute('''
-    CREATE TABLE IF NOT EXISTS results (
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS results(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        height REAL,
-        case_name TEXT,
-        impact_energy REAL
+        users INTEGER,
+        stock INTEGER,
+        response_time REAL,
+        failure_rate REAL,
+        oversell INTEGER,
+        cache INTEGER,
+        queue INTEGER,
+        load_balancer INTEGER
     )
-    ''')
+    """)
 
     conn.commit()
     conn.close()
@@ -26,39 +32,64 @@ def save():
 
     data = request.json
 
-    conn = sqlite3.connect('simulation.db')
+    conn = sqlite3.connect("simulation.db")
     cur = conn.cursor()
 
-    cur.execute(
-        "INSERT INTO results(height, case_name, impact_energy) VALUES (?, ?, ?)",
-        (
-            data['height'],
-            data['case_name'],
-            data['impact_energy']
-        )
+    cur.execute("""
+    INSERT INTO results(
+        users,
+        stock,
+        response_time,
+        failure_rate,
+        oversell,
+        cache,
+        queue,
+        load_balancer
     )
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    """,
+    (
+        data['users'],
+        data['stock'],
+        data['responseTime'],
+        data['failureRate'],
+        data['oversell'],
+        int(data['cache']),
+        int(data['queue']),
+        int(data['loadBalancer'])
+    ))
 
     conn.commit()
     conn.close()
 
-    return jsonify({"status":"ok"})
+    return jsonify({"status":"success"})
 
 
 @app.route('/results')
 def results():
 
-    conn = sqlite3.connect('simulation.db')
+    conn = sqlite3.connect("simulation.db")
     cur = conn.cursor()
 
-    cur.execute(
-        "SELECT height, case_name, impact_energy FROM results"
-    )
+    cur.execute("""
+    SELECT
+        users,
+        stock,
+        response_time,
+        failure_rate,
+        oversell
+    FROM results
+    """)
 
     rows = cur.fetchall()
 
     conn.close()
 
     return jsonify(rows)
+
+@app.route('/chart')
+def chart():
+    return render_template('chart.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
